0ce0f36c6769e47e0b6c2e00745ef844e57b424a
shakil islam <freelancershakil913@gmail.com>
2022-01-04 00:50:24 +0600

    internal css in jsx

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const ToDoTitle = "To Do App";
-const ToDoDesc = "lorem ipsum dolor sit amet lorem ipsum dolor sit amet";
-const date = new Date();
-const Today = date.getDate();
-const month = date.getMonth();
-const CurrentFullYear = date.getFullYear();
+
+ const headingStyle = {
+  color: 'red',
+}
 
 ReactDOM.render(
   <div>
-    <h1 style={{color:'red'}}>{ToDoTitle}</h1>
-    <h2>{ToDoDesc}</h2>
-    <h2>{Today + "/" + month + "/" + CurrentFullYear}</h2>
+    <h1 style={headingStyle}>hello world</h1>
+   
   </div>,
   document.getElementById('root')
 );
