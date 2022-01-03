067dc8a423e6cf7a50755a17a058194ee667bb63
shakil islam <freelancershakil913@gmail.com>
2022-01-03 23:56:59 +0600

    type jsx

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  "Hello world",
+  <div>
+    <h1>To Do App</h1>
+    <h1>To Do Title</h1>
+    <p>lorem ipsum dolor sit amet lorem ipsum dolor sit amet</p>
+  </div>,
   document.getElementById('root')
 );
 
