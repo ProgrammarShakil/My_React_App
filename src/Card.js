c9513973de6b672761bfc114c4d7b0e52f2f7658
shakil islam <freelancershakil913@gmail.com>
2022-01-04 01:30:56 +0600

    make component from external file

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+const todoTitle = "Call Family";
+const todoDesc = "Ipsum no sea sadipscing consetetur vero. Nonumy justo diam sed lorem sit, sed sit clita sit takimata sed sanctus invidunt.";
+const fullDate = new Date();
+const date = fullDate.getDate() + "/" + fullDate.getMonth() + "/"+ fullDate.getFullYear();
+
+
+function Card(){
+  return     <div  className="card-style">
+                <h3>{todoTitle}</h3>
+                <p>{todoDesc}</p>
+                <p>{date}</p>
+             </div>
+}
+
+export default Card;
