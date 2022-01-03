2ae3638216f6df13260711feaabf1820e479f667
shakil islam <freelancershakil913@gmail.com>
2022-01-04 05:04:48 +0600

    State and setState with Constructor

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import  './index.css';
 import Data from './data.json';
 import Card2 from './Components/Card2';
+import STATE from './Components/STATE';
 
 function App() {
 
    return <div>
-            <Card2 name='class component here' />
+            <STATE />
          </div>
 }
 
