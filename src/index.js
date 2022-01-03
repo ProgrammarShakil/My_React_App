e1da4497a1e97ac681d01da1273f19c09cc2436d
shakil islam <freelancershakil913@gmail.com>
2022-01-04 00:55:21 +0600

    external css in public

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
- const headingStyle = {
-  color: 'red',
-}
-
 ReactDOM.render(
   <div>
-    <h1 style={headingStyle}>hello world</h1>
+    <h1 className='headingStyle'>hello world</h1>
    
   </div>,
   document.getElementById('root')
