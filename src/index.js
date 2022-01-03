cd367726d9aedd9c3f17e1a560924a63cecd8843
shakil islam <freelancershakil913@gmail.com>
2022-01-04 01:16:11 +0600

    made a card in index.js file

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const todoTitle = "Call Family"
+const todoDesc = "Ipsum no sea sadipscing consetetur vero. Nonumy justo diam sed lorem sit, sed sit clita sit takimata sed sanctus invidunt."
+const fullDate = new Date();
+const date = fullDate.getDate() + "/" + fullDate.getMonth() + "/"+ fullDate.getFullYear();
+
 ReactDOM.render(
   <div>
-    <h1 className='headingStyle'>hello world</h1>
-   
+    <div  className="card-style">
+        <h3>{todoTitle}</h3>
+        <p>{todoDesc}</p>
+        <p>{date}</p>
+    </div>
   </div>,
   document.getElementById('root')
 );
